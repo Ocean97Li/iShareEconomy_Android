731935a8058d81014f9f9d4c6b0aed4fab76e0a7
Ocean Li <ocean97li@gmail.com>
2019-12-10 15:55:53 +0100

    Add LendoBjectTypeEnum
    Also added documentation for LendObject

---
+++ app/src/main/java/com/ocean/ishareeconomy_android/models/LendObjectType.kt
@@ -0,0 +1,5 @@
+package com.ocean.ishareeconomy_android.models
+
+enum class LendObjectType {
+    Object, Service, Transportation
+}
--- app/src/main/java/com/ocean/ishareeconomy_android/models/LendingObject.kt
+++ app/src/main/java/com/ocean/ishareeconomy_android/models/LendingObject.kt
@@ -1,10 +1,21 @@
 package com.ocean.ishareeconomy_android.models
-
+/**
+ * Part of *models*.
+ *
+ * LendObject is an object, serive or transportation mode that one user lends out and that other users can use
+ * @property id the lendobject's id
+ * @property name the name of the lendobject
+ * @property description a short description text
+ * @property type the type: Object, Service or Transportation
+ * @property owner refers to the User that is the lendobject's owner
+ * @property user refers to User that is the lendobject's current user
+ * @property waitingList a list of Users that is the lendobject's future users
+ */
 class LendingObject(
     id: String,
     name: String,
     description: String,
-    type: String,
+    type: LendObjectType,
     owner: ObjectOwner,
     user: ObjectUser,
     waitingList: List<LendingObject>
