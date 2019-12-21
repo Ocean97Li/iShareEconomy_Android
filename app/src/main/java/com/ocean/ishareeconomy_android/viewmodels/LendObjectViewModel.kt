eafd531af45256cc4733aefdbc86464fed0ae56b
Ocean Li <ocean97li@gmail.com>
2019-12-21 15:12:31 +0100

    Fix LendObject Icon
    Changes based on type

--- app/src/main/java/com/ocean/ishareeconomy_android/viewmodels/LendObjectViewModel.kt
+++ app/src/main/java/com/ocean/ishareeconomy_android/viewmodels/LendObjectViewModel.kt
@@ -1,8 +1,24 @@
 package com.ocean.ishareeconomy_android.viewmodels
 
 import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import com.ocean.ishareeconomy_android.R
+import com.ocean.ishareeconomy_android.models.LendObjectType
 import com.ocean.ishareeconomy_android.models.LendingObject
 
+
 class LendObjectViewModel(
-    val data: LendingObject, val color: ColorDrawable
-)
+    private val data: LendingObject, val color: ColorDrawable
+) {
+    val name = data.name
+    val description = data.description
+    val numberOfUsers = Integer.toString(data.waitingList.size + if (data.user != null) 1 else 0)
+
+    fun type() : Int {
+        when(data.lendObjectType) {
+            LendObjectType.Tool -> return R.drawable.ic_lendobject_tool_solid
+            LendObjectType.Service -> return R.drawable.ic_lendobject_service_solid
+            LendObjectType.Transportation -> return R.drawable.ic_lendobject_transportation_solid
+        }
+    }
+}
