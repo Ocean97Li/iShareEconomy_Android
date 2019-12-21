d6f25aacb8af667f6335eff5a3af600942d07f8c
Ocean Li <ocean97li@gmail.com>
2019-12-21 15:32:28 +0100

    Isolate Network Services

--- app/src/main/java/com/ocean/ishareeconomy_android/network/Service.kt
+++ app/src/main/java/com/ocean/ishareeconomy_android/network/Service.kt
@@ -1,26 +1,10 @@
 package com.ocean.ishareeconomy_android.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.ocean.ishareeconomy_android.models.User
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import kotlinx.coroutines.Deferred
-import retrofit2.Call
-import retrofit2.Response
+import com.ocean.ishareeconomy_android.network.services.LendingService
+import com.ocean.ishareeconomy_android.network.services.UserService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.*
-
-interface UserService {
-
-    @GET("users/{id}/users")
-    fun getUsersAsync(@Path("id") id: String, @Header("Authorization") auth: String): Deferred<Response<List<User>>>
-
-    @GET("users/{id}/users")
-    fun getUsers(@Path("id") id: String, @Header("Authorization") auth: String): Call<List<User>>
-}
-
 
 /**
  * Main entry point for network access. Call like `Network.users.getUsers(id, auth)`
@@ -35,4 +19,5 @@ object Network {
         .build()
 
     val users = retrofit.create(UserService::class.java)
+    val lending = retrofit.create(LendingService::class.java)
 }
